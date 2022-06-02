3a9c501f1025362a3fc8b7c35820a41b8df48d72
Medusas71 <medusas71@icloud.com>
2022-06-02 02:35:55 +0000

    Delete recipe from db

--- app.py
+++ app.py
@@ -169,6 +169,16 @@ def edit_recipe(recipe_id):
         "edit_recipe.html", recipe=recipes, categories=categories)
 
 
+@app.route("/delete_recipe/<recipe_id>")
+def delete_recipe(recipe_id):
+    """
+    allows user to delete a recipe in the db
+    """
+    mongo.db.recipes.delete_one({"_id": ObjectId(recipe_id)})
+    flash("Recipe Successfully Deleted", category="green")
+    return redirect(url_for("get_recipes"))
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
